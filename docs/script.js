3fe2293c3fde455b158b2aee439ddcc986630fb9
atoy322 <atoy_0322@yahoo.co.jp>
2023-08-02 00:27:08 +0900

    update doc

---
+++ docs/script.js
@@ -0,0 +1,21 @@
+function onload() {
+    var classes = document.getElementsByClassName("code");
+
+    for(var i=0; i<classes.length; i++) {
+        var elem = classes[i];
+        set(elem);
+    }
+}
+
+function set(elem) {
+    var src = elem.getAttribute("src");
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", src);
+    xhr.send();
+
+    console.log(elem);
+
+    xhr.onload = function() {
+        elem.innerHTML = xhr.responseText;
+    }
+}
